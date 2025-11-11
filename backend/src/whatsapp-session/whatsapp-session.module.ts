a2d71211a8ef9bb451a81287d7e2605952f8aa8d
ragul.se21 <ragul.se21@bitsathy.ac.in>
2025-11-11 18:08:34 +0530

    Auto replay fix

--- backend/src/whatsapp-session/whatsapp-session.module.ts
+++ backend/src/whatsapp-session/whatsapp-session.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WhatsappSessionService } from './whatsapp-session.service';
+import { AutoReplyModule } from '../auto-reply.module';
 
 @Module({
+  imports: [AutoReplyModule],
   providers: [WhatsappSessionService],
   exports: [WhatsappSessionService]
 })

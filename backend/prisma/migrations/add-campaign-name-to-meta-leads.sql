6355c4458c7c7b50d0ac6ec5225c2b36aed56d09
naveenv0906 <naveenv0906@gmail.com>
2026-05-11 19:43:21 +0530

    unwanted sql file deleted

--- backend/prisma/migrations/add-campaign-name-to-meta-leads.sql
+++
@@ -1,5 +0,0 @@
--- Add campaignName column to MetaLead table
-ALTER TABLE "MetaLead" ADD COLUMN IF NOT EXISTS "campaignName" TEXT;
-
--- Create index on campaignName for faster filtering
-CREATE INDEX IF NOT EXISTS "MetaLead_campaignName_idx" ON "MetaLead"("campaignName");
--- backend/update-tenant-phonenumberid.sql
+++
@@ -1,5 +0,0 @@
--- Update tenant with phoneNumberId from Master Config
--- Run this on whatsapp_central database
-UPDATE "Tenant" 
-SET "phoneNumberId" = '803957376127788' 
-WHERE id = 2;
